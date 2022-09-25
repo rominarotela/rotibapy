3f53cb1d9e6198db71c58e9efea12a409e3618a0
rominarotela <romy_rotela@fpuna.edu.py>
2022-09-24 21:13:40 -0400

    Efecto parallax

--- js/efectos.js
+++ js/efectos.js
@@ -58,5 +58,4 @@ $(document).ready(function() {
         }, 500);
     });
 
-
 });
---
+++ js/parallax.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(window).scroll(function(){
+        let windowWidth = $(window).width();
+
+        if(windowWidth > 800) {
+            let scroll = $(window).scrollTop();
+            
+            $('header .textos').css({
+                'transform': 'translate(0px,' + scroll/3 + '%)'
+            });
+
+            $('.acerca-de article').css({
+                'transform': 'translate(0px, -' + scroll/4 + '%)'
+            });
+        }
+    });
+
+    $(window).resize(function() {
+        let windowWidth = $(window).width();
+
+        if(windowWidth < 800) {
+            $('.acerca-de article').css({
+                'transform': 'translate(0px, 0px)'
+            });
+        }
+    })
+});
